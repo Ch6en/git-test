f7320067e6b5f3b65245127cfae469710fbcabc8
Chen <Chen@xl.com>
2022-06-06 21:40:56 +0800

    five src

--- src/main/java/com/chen/demo/GitTest.java
+++ src/main/java/com/chen/demo/GitTest.java
@@ -5,5 +5,8 @@ public class GitTest {
         System.out.println("hello world git-1");
         System.out.println("hello world git-2");
         System.out.println("hello world git-3");
+        System.out.println("hello world git-4");
+        System.out.println("hello world git-5");
+        System.out.println("hello world git-6");
     }
 }
