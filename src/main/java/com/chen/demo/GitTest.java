9767dbc7b0fe01dd51cf15b575727e5d56dc6076
Chen <Chen@xl.com>
2022-06-06 21:34:52 +0800

    one src/

---
+++ src/main/java/com/chen/demo/GitTest.java
@@ -0,0 +1,8 @@
+package com.chen.demo;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello world git-1");
+        System.out.println("hello world git-2");
+    }
+}
